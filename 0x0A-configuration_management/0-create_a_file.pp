f8dbf00472f4623943da1156e726aa6e70e962f6
Ron David <ololoron@gmail.com>
2024-02-28 21:27:02 +0300

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create file in /temp
+
+file { '/temp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}

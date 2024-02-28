8262b4809cd2de183d5c6d379aaf64b0b5161f7a
Ron David <ololoron@gmail.com>
2024-02-28 21:31:54 +0300

    Things fall into place'

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,5 @@
-# Create file in /temp
-
-file { '/temp/school':
+# This manifest creates a file at /tmp
+file { '/tmp/school':
     ensure  => 'file',
     owner   => 'www-data',
     group   => 'www-data',
